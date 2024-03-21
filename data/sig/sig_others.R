936c531e0d1be4424632021c87b944997ca39449
lindandan199113 <164142493+lindandan199113@users.noreply.github.com>
2024-03-21 10:47:02 +0800

    Add files via upload

---
+++ data/sig/sig_others.R
@@ -0,0 +1,58 @@
+
+
+# IMS.Sig
+
+IMS.Sig <-  c('CCL8', 'VCAN', 'CCL2', 'BCAT1', 'ISG15', 'CD163', 'AXL','CCL13','COL6A3','SIGLEC1', 'PDGFRB', 'IL10', 'STC1','ADAM12', 'OLFML2B','FAP','TWIST2', 'INHBA')  
+
+# TRS.Sig
+
+TRS.Sig <- c('CTLA4', 'CXCR6', 'LYST', 'CD38', 'GBP2','HLA-DRB5')
+
+# NLRP3.Sig
+
+NLRP3.Sig <- c('ARRDC1-AS1','CARD8','GSDMD','ATAT1','CD36','CPTP','DHX33','EIF2AK2','GBP5','NLRC3','PYDC2','SIRT2','TLR4','TLR6','USP50','APP','CASP1','HSP90AB1','MEFV','NFKB1','NFKB2','NLRP3','P2RX7','PANX1','PSTPIP1','PYCARD','RELA','SUGT1','TXN','TXNIP')
+
+
+# IFNG.Sig
+
+IFNG.Sig <- c('IFNG', 'STAT1', 'IDO1', 'CXCL10', 'CXCL9', 'HLA-DRA')
+
+
+# CRMA.Sig 
+
+CRMA.Sig <- c('CSAG1','CSAG2','CSAG3', 'MAGEA2', 'MAGEA2B', 'MAGEA3', 'MAGEA6', 'MAGEA12')
+
+
+# T.cell.inflamed.Sig
+
+T.cell.inflamed.Sig <- c('CD3D','IDO1','CIITA','CD3E','CCL5','GZMK','CD2','HLA-DRA','CXCL13','IL2RG','NKG7','HLA-E','CXCR6','LAG3','TAGAP','CXCL10','STAT1','GZMB')
+
+
+
+# Cytotoxic.Sig
+
+Cytotoxic.Sig <- c("GZMA", "PRF1")
+Cytotoxic.Sig
+
+# PDL1.Sig
+
+PDL1.Sig <- c('PDL1','PDCD1')
+
+# LRRC15.CAF.Sig
+
+LRRC15.CAF.Sig <- c('MMP11','COL11A1','C1QTNF3','CTHRC1','COL12A1','COL10A1','COL5A2','GJB2','THBS2','AEBP1','MFAP2','LRRC15','PLAU','ITGA11') # Alias for 'PRLHR' are:'GR3','GPR10','PrRPR'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
